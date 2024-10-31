bde9740c82229520a20abfea06d3639d3b81a9a8
MgreytakUW <mgreytak@cs.washington.edu>
2024-10-30 23:44:27 -0700

    Audio and SFX busses
    Added separate busses and volume controls.

--- UI/Scenes/notebook.gd
+++ UI/Scenes/notebook.gd
@@ -3,18 +3,23 @@ extends Node2D
 @onready var page_2: Control = $NotebookImgCont/Notebook/Page2
 @onready var button_r: TextureButton = $ButtonR
 @onready var button_l: TextureButton = $ButtonL
-@onready var h_slider_vol: HSlider = $NotebookImgCont/Notebook/Page2/LeftPageCont/HSlider_vol
 @onready var check_box_violence: CheckBox = $NotebookImgCont/Notebook/Page2/LeftPageCont/CheckBox_violence
-@onready var vol_level: RichTextLabel = $NotebookImgCont/Notebook/Page2/LeftPageCont/HBoxContainer2/VolLevel
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var vol_level_mus: RichTextLabel = %VolLevelMus
+@onready var h_slider_vol_mus: HSlider = %HSlider_vol_mus
+@onready var vol_level_sfx: RichTextLabel = %VolLevelSFX
+@onready var h_slider_vol_sfx: HSlider = %HSlider_vol_sfx
+@onready var page_turn_player: AudioStreamPlayer = $PageTurnPlayer
 
 
 var pg = 1;
-var vol = 100;
+var vol_mus = 100;
+var vol_sfx = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	h_slider_vol.value = vol
+	h_slider_vol_mus.value = vol_mus
+	h_slider_vol_sfx.value = vol_mus
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -29,16 +34,27 @@ func _process(_delta: float) -> void:
 			page_2.visible = true
 			button_l.visible = true
 			button_r.visible = false
-	vol_level.text = str(vol,"%")
+	vol_level_mus.text = str(vol_mus,"%")
+	vol_level_sfx.text = str(vol_sfx,"%")
 
 func _on_button_r_pressed():
 	pg += 1
+	page_turn_player.play()
+	
 	
 
 func _on_button_l_pressed():
 	pg -= 1
-	
+	page_turn_player.play()
 
-func _on_h_slider_vol_drag_ended(_value_changed: bool) -> void:
-	vol = h_slider_vol.value
-	audio_stream_player.volume_db = linear_to_db(vol/100)
+func _on_h_slider_vol_mus_drag_ended(value_changed: bool) -> void:
+	vol_mus = h_slider_vol_mus.value
+	var mus_idx = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(mus_idx, linear_to_db(vol_mus/100))
+
+
+
+func _on_h_slider_vol_sfx_drag_ended(value_changed: bool) -> void:
+	vol_sfx = h_slider_vol_sfx.value
+	var sfx_idx = AudioServer.get_bus_index("SFX") 
+	AudioServer.set_bus_volume_db(sfx_idx, linear_to_db(vol_sfx/100))
