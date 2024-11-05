e25572f97a43d227ef1d7f9d3ac4761bec850a94
Matthew Brosnon <32721082+pwnies2012@users.noreply.github.com>
2024-11-04 16:48:10 -0800

    Refactor vignette, add sprite & idle anim

--- Anxiety Effects/Vignette/vignette_anim.gd
+++ Anxiety Effects/Vignette/vignette_anim.gd
@@ -3,5 +3,7 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	speed_scale = 0.5
-	play("vignette_constriction_1")
+	speed_scale = 1
+	play("vignette_constriction")
+	queue("vignette_idle")
+	
--- Anxiety Effects/Vignette/vignette_handler.gd
+++ Anxiety Effects/Vignette/vignette_handler.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@onready var node = $Vignette
+@onready var anim = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_scale(Vector2(1.25, 1.25))
+	# apply_scale(Vector2(1.25, 1.25))
 	global_position = Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
-	
-	
 	
