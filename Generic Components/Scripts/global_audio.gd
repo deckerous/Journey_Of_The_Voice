5b2d97c931e36189721857b75c61c9e24378d622
Matthew Brosnon <pwnies2012@gmail.com>
2024-11-11 16:21:46 -0800

    add ID functionality

--- Generic Components/Scripts/global_audio.gd
+++ Generic Components/Scripts/global_audio.gd
@@ -8,7 +8,7 @@ func set_global_vol(val):
 		if audio is AudioStreamPlayer:
 			audio.volume_db = linear_to_db(global_vol/100)
 
-func play_sound(audio_stream: AudioStream):
+func play_sound(audio_stream: AudioStream) -> AudioStreamPlayer:
 	var temp_stream = AudioStreamPlayer.new()
 	temp_stream.stream = audio_stream
 	temp_stream.autoplay = true
@@ -16,6 +16,19 @@ func play_sound(audio_stream: AudioStream):
 	temp_stream.finished.connect(finished)
 	
 	add_child(temp_stream)
+	return temp_stream
+	
+func play_sound_id(audio_stream: AudioStream, id: String) -> AudioStreamPlayer:
+	var temp_stream = play_sound(audio_stream)
+	temp_stream.name = id
+	
+	return temp_stream
+	
+func get_stream_from_id(id: String) -> AudioStreamPlayer:
+	return find_child(id)
+
+func stop_stream_from_id(id: String):
+	remove_child(find_child(id))
 
 func finished():
 	for audio in get_children():
