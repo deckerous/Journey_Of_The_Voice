d8b9a4a18973900232ebd7cdac9014fb33820a48
bcorrea1006 <benitoscorrea@gmail.com>
2024-12-05 21:26:51 -0800

    music transitions with scenes now

--- Generic Components/Scripts/chapter.gd
+++ Generic Components/Scripts/chapter.gd
@@ -12,6 +12,9 @@ var save_path = TEST_SAVE_PATH
 
 @onready var continue_button: Button = $CanvasLayer/ContinueButton
 
+# Alerts other nodes when we're changing chapters
+signal clicked
+
 func _ready() -> void:
 	if area != null:
 		area.area_complete.connect(unhide_continue_button)
@@ -21,6 +24,8 @@ func unhide_continue_button():
 	continue_button.visible = true
 
 func go_to_next_chapter():
+	# Alert other nodes that we're switching chapters
+	clicked.emit()
 	Player.update_chapter(chapter_number + 1)
 	Player.write_save(save_path)
 	get_tree().change_scene_to_packed(next_chapter)
