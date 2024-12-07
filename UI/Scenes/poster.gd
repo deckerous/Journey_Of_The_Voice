8f6ae93a5eccca3bbb4043bd041422ab84c2a091
Tracy Sheng <tsheng@cs.washington.edu>
2024-12-07 12:17:11 -0800

    added poster script idk why it wasnt there sorry guys

---
+++ UI/Scenes/poster.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var poster_button: TextureButton = $PosterButton
+@onready var background: ColorRect = $CanvasLayer/Background
+@onready var poster: HBoxContainer = $CanvasLayer/Poster
+@onready var back_button: TextureButton = $CanvasLayer/BackButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	background.hide()
+	poster.hide()
+	back_button.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_poster_button_pressed() -> void:
+	background.show()
+	poster.show()
+	back_button.show()
+
+func _on_back_button_pressed() -> void:
+	background.hide()
+	poster.hide()
+	back_button.hide()
